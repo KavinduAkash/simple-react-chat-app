42988c756200a461c58659a8cad7909d455c4b68
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-02-28 12:25:53 +0530

    apply new style to app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Message from "./Message";
+import {Button, Icon, Input} from "semantic-ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+    state = {
+        message: "",
+        message_arr: [
+            {
+                username:'jhone',
+                message: 'hello!',
+                timestamp: '20:00:00'
+            }
+        ],
+        user:""
+    };
+
+    componentDidMount() {
+        this.setState({
+            user: prompt("What is your username?")
+        });
+
+    }
+
+    sendMessage = (event) => {
+        event.preventDefault();
+        if(this.state.message!=="") {
+            let obj = {
+                username: this.state.user,
+                message: this.state.message,
+                timestamp: new Date()
+            };
+
+            let arr = this.state.message_arr;
+            arr.push(obj);
+            this.setState({
+                // message_arr: [...this.state.message_arr,obj],
+                message_arr: arr,
+                message: ""
+            });
+        } else {
+            alert("Please, Enter a message!")
+        }
+    };
+
+  render() {
+      let message_arr = this.state.message_arr;
+    return(
+        <div className={'chat-body'}>
+            <div className={'x'}>
+                <h2>Simple React Messenger App</h2>
+            </div>
+            <div className={'text-center'}>
+                <form>
+                    <div>
+                        <Input type="text" value={this.state.message} onChange={event=>this.setState({message:event.target.value})}/>
+                        <Button circular onClick={this.sendMessage} icon='send'></Button>
+                    </div>
+                </form>
+                <div className={'all-msg'}>
+                    {
+                        message_arr.map(res=><Message user={this.state.user} data={res}/>)
+                    }
+                </div>
+            </div>
+        </div>
+    )
+  };
 }
 
 export default App;
---
+++ src/Message.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './message.style.scss';
+import {Card, Icon} from 'semantic-ui-react'
+
+class Message extends React.Component {
+    render() {
+        let data = this.props.data;
+        let user = this.props.user;
+        return(
+            <div className={`message ${data.username===user?'user-message':'guest-message'}`}>
+                <div className={'message-head text-center'}>
+                    <div>{data.username}</div>
+                    <Icon name='circle' />
+                    <div>{data.timestamp.toString()}</div>
+                </div>
+                <div className={'message-body'}>{data.message}</div>
+            </div>
+        )
+    }
+}
+
+export default Message;
